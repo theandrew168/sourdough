9eb4536cb0eb0f4908b1cfb9fa196eb6f362f0f6
theandrew168 <andrew@shallowbrooksoftware.com>
2022-08-17 15:26:50 -0500

    Organize things a bit

--- frontend/vertexformat.ts
+++ frontend/vertexformat.ts
@@ -7,6 +7,15 @@ export enum VertexType {
 
 export type VertexSize = 1 | 2 | 3 | 4;
 
+export function toVertexSize(size: number): VertexSize {
+	const valid = size in [1, 2, 3, 4];
+	if (!valid) {
+		throw new Error(`invalid VertexSize: ${size}`);
+	}
+	return size as VertexSize;
+}
+
+// this assumes that the primitive type is always a 32-bit float
 export type VertexComponent = {
 	type: VertexType;
 	size: VertexSize;
@@ -20,12 +29,4 @@ export function vertexFormatSize(format: VertexFormat): number {
 
 export function vertexFormatStride(format: VertexFormat): number {
 	return vertexFormatSize(format) * 4;
-}
-
-export function toVertexSize(size: number): VertexSize {
-	const valid = size in [1, 2, 3, 4];
-	if (!valid) {
-		throw new Error(`invalid VertexSize: ${size}`);
-	}
-	return size as VertexSize;
 }
