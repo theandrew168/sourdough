b809e6cb2ac792b5ced7b26307a288e3840a97bb
theandrew168 <andrew@shallowbrooksoftware.com>
2022-08-21 20:27:02 -0500

    Add placeholder for square app

--- frontend/main.ts
+++ frontend/main.ts
@@ -1,6 +1,8 @@
 import * as triangle from './app/triangle';
+import * as square from './app/square';
 import * as box from './app/box';
 
 // export each app entrypoint at the global scope
-(globalThis as any).triangleMain = triangle.main;
-(globalThis as any).boxMain = box.main;
+(globalThis as any).triangle = triangle.main;
+(globalThis as any).square = square.main;
+(globalThis as any).box = box.main;
